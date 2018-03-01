ab890221187adebd5cf6cf176cfb6fe458f996ca
Michael Merritt <mmerritt1010@gmail.com>
2018-03-01 14:17:36 -0800

    Add envvar check & Update README.md

--- send-ethereum-transaction/README.md
+++ send-ethereum-transaction/README.md
@@ -1,9 +1,11 @@
-# Generate Ethereum Transactions offline and Send them to Testnet or Mainnet
+## Generate Ethereum transactions offline then send them to the Network
 
-### import private key from file locally
-```export PKEY=$( cat ./test_wallet_pkey)```
+* Visit https://infura.io/ to obtain an access token
+* Place your private key in a file called wallet_pkey in the current directory. This will be used to generate your transaction offline.
+* Configure your send and receiving ethereum addresses in the script
 
-Configure your send and receiving ethereum addresses in the script
+#### Export envvars needed to run
+```export PKEY=$( cat ./wallet_pkey) && export INFURA_ACCESS_TOKEN='access-token'```
 
-### Send Transaction
+#### Generate & send transaction
 ```node send-ethereum.js```
--- send-ethereum-transaction/send-ethereum.js
+++ send-ethereum-transaction/send-ethereum.js
@@ -1,5 +1,10 @@
-var mainnet = "https://mainnet.infura.io/YQA9WLaI48uL3JeX375x"
-var testnet = "https://ropsten.infura.io/YQA9WLaI48uL3JeX375x"
+if (process.env.PKEY && process.env.INFURA_ACCESS_TOKEN) {
+    var mainnet = "https://mainnet.infura.io/" + (process.env.INFURA_ACCESS_TOKEN)
+    var testnet = "https://ropsten.infura.io/" + (process.env.INFURA_ACCESS_TOKEN)
+    var pkey1 = (process.env.PKEY)
+  } else {
+    throw Error('environment variables INFURA_ACCESS_TOKEN or PKEY not set')
+}
 
 var Web3 = require("web3")
 var EthTx = require("ethereumjs-tx")
@@ -8,9 +13,10 @@ var web3 = new Web3(new Web3.providers.HttpProvider(mainnet))
 var addr1 = "0x59ca0e84a5a2fded343104733120f88023c3b41b"
 var addr2 = "0x2c32fb487da2ee0485b9ce8d3edc982e04ca0973"
 
+console.log(web3.utils.toHex(web3.eth.getTransactionCount(addr1)))
+
 var rawTx = {
-    // nonce: web3.utils.toHex(web3.eth.getTransactionCount(addr1)),
-    nonce: 0,
+    nonce: web3.utils.toHex(web3.eth.getTransactionCount(addr1)),
     to: addr2,
     gasPrice: web3.utils.toHex(21000000000),
     gasLimit: web3.utils.toHex(21000),
@@ -18,9 +24,7 @@ var rawTx = {
     data: ""
 }
 
-var pkey1 = (process.env.PKEY)
 var pkey1x = new Buffer(pkey1, 'hex')
-
 var tx = new EthTx(rawTx)
 tx.sign(pkey1x)
 
